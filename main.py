d8ee2ebcc6069c933904a101385f63b5346804d4
BRUNO CESAR <bcesar.g6@gmail.com>
2015-07-23 21:33:22 -0300

    deus me odeia

---
+++ main.py
@@ -0,0 +1,119 @@
+import StringIO
+import json
+import logging
+import random
+import urllib
+import urllib2
+
+# for sending images
+from PIL import Image
+import multipart
+
+# standard app engine imports
+from google.appengine.api import urlfetch
+from google.appengine.ext import ndb
+import webapp2
+
+# Nossos imports
+import cmds
+import conversa
+
+TOKEN = '105794279:AAEZQkZX-HnXHMBG8NHkc0CWyDjvpOnHM-U'
+
+BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
+
+class MeHandler(webapp2.RequestHandler):
+    def get(self):
+        urlfetch.set_default_fetch_deadline(60)
+        self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'getMe'))))
+
+
+class GetUpdatesHandler(webapp2.RequestHandler):
+    def get(self):
+        urlfetch.set_default_fetch_deadline(60)
+        self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'getUpdates'))))
+
+
+class SetWebhookHandler(webapp2.RequestHandler):
+    def get(self):
+        urlfetch.set_default_fetch_deadline(60)
+        url = self.request.get('url')
+        if url:
+            self.response.write(json.dumps(json.load(urllib2.urlopen(BASE_URL + 'setWebhook', urllib.urlencode({'url': url})))))
+
+
+class WebhookHandler(webapp2.RequestHandler):
+    def post(self):
+        urlfetch.set_default_fetch_deadline(60)
+        body = json.loads(self.request.body)
+        logging.info('request body:')
+        logging.info(body)
+        self.response.write(json.dumps(body))
+
+        update_id = body['update_id']
+        message = body['message']
+        message_id = message.get('message_id')
+        date = message.get('date')
+        text = message.get('text')
+        fr = message.get('from')
+        chat = message['chat']
+        chat_id = chat['id']
+        user_id = message['from']
+        uId = user_id.get('id')    #gets user id
+        if not text:
+            logging.info('no text')
+            return
+
+        def reply(msg=None, img=None):
+            if msg:
+                resp = urllib2.urlopen(BASE_URL + 'sendMessage', urllib.urlencode({
+                    'chat_id': str(chat_id),
+                    'text': msg.encode('utf-8'),
+                    'disable_web_page_preview': 'true',
+                    'reply_to_message_id': str(message_id),
+                })).read()
+            elif img:
+                resp = multipart.post_multipart(BASE_URL + 'sendPhoto', [
+                    ('chat_id', str(chat_id)),
+                    ('reply_to_message_id', str(message_id)),
+                ], [
+                    ('photo', 'image.jpg', img),
+                ])
+            else:
+                logging.error('no msg or img specified')
+                resp = None
+
+            logging.info('send response:')
+            logging.info(resp)
+        if text.startswith('/'):
+            cc = cmds.comandos #calls the Cmd class
+            if text.startswith('/meme') or text.startswith('/image'):
+                rpl = cc(text, chat_id)
+                if rpl.startswith('avaibles') or rpl.startswith('there'):
+                    reply(rpl)
+                else:
+                    reply(img=rpl)
+            else:
+                rpl = cc(text, chat_id)
+                reply(rpl)
+        if text.startswith('@'):
+            reply('nao aceitamos arroba')
+        #Aqui estao os algoritmos para as respostas sem serem comandos.
+
+        else:
+            cc = conversa.responde
+            get = cmds.getEnabled
+            if get(chat_id):
+                rpl = cc(text)
+                reply(rpl)
+            else:
+                reply('bot desligado')
+                logging.info('not enabled for chat_id {}'.format(chat_id))
+
+
+app = webapp2.WSGIApplication([
+    ('/me', MeHandler),
+    ('/updates', GetUpdatesHandler),
+    ('/set_webhook', SetWebhookHandler),
+    ('/webhook', WebhookHandler),
+], debug=True)
