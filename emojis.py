9182287fba9f8ddf8cc6505a8095b807efd65856
BRUNO CESAR <bcesar.g6@gmail.com>
2015-09-16 00:45:49 -0300

    resolvendo problemas de versionamento

---
+++ emojis.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+#Linguagem Emoji, contém os códigos referentes aos Emojis
+
+emoji_heart = (u'\u2764\ufe0f').encode('utf-8')
+emoji_heartb = (u'\U0001f494').encode('utf-8')
+emoji_confetti = (u'\U0001f389').encode('utf-8')
+emoji_claps = (u'\U0001f44f\U0001f3fc').encode('utf-8')
+emoji_feliz = (u'\U0001f601').encode('utf-8')
+emoji_lua = (u'\U0001f31a').encode('utf-8')
+emoji_coroa = (u'\U0001f451').encode('utf-8')
+emoji_sorriso = (u'\U0001f601').encode('utf-8')
+emoji_blink = (u'\U0001f609').encode('utf-8')
